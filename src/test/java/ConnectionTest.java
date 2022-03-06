68d2337c65ac088129e6acef8015a2867882244d
小罗颂 <3035728224@qq.com>
2022-03-06 16:09:35 +0800

    first commit

--- src/test/java/ConnectionTest.java
+++ src/test/java/ConnectionTest.java
@@ -10,5 +10,6 @@ public class ConnectionTest {
     public static void main(String[] args) {
         Connection connection = DataBaseConnection.getConnection();
         System.out.println(connection);
+        System.out.println("011111");
     }
 }
