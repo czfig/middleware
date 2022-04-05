b21e802f464569d7624816a157bb3d033ff59466
小罗颂 <85983832+czfig@users.noreply.github.com>
2022-04-05 10:42:28 +0800

    Update ConnectionTest.java

--- src/test/java/ConnectionTest.java
+++ src/test/java/ConnectionTest.java
@@ -2,10 +2,6 @@ import connection.DataBaseConnection;
 
 import java.sql.Connection;
 
-/**
- * @author jiang
- * @date 2022/3/2 21:53
- */
 public class ConnectionTest {
     public static void main(String[] args) {
         Connection connection = DataBaseConnection.getConnection();
